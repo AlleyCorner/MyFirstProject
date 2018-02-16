36eb109c562220d035169da91c5f24de2048107b
AlleyCorner <13921778762@163.com>
2018-02-16 10:51:53 +0800

    1.0

---
+++ word_remember.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+my_word = {}
+choice = int(input("Please choose the choice:"))
+if choice = 1:
+    input_words()
+elif choice = 2:
+    test_words()
+elif choice = 3:
+    show_the_false_words()
+
+
+
+def input_words()
+    key = input("Please input the words:")
+    value = input("Please input the translation:")
+    my_word[str(key)] = str(value)
+
+if __name__ == "__main__":
+    pass
